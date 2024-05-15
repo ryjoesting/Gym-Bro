5299a4afd16cbb71b01c880d8518997603e4a4f3
Ryan Joesting <rmjoesting@crimson.ua.edu>
2024-05-15 14:35:47 -0400

    Update header color

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -3,11 +3,14 @@ import { Colors } from 'react-native-ui-lib';
 
 export default function _layout() {
     return (
-        <Stack screenOptions={{headerShown: true}}>
+        <Stack screenOptions={{
+            headerShown: true, 
+            headerStyle: {backgroundColor: Colors.$backgroundPrimaryHeavy},
+            headerTintColor: Colors.$textDefaultLight
+        }}>
             <Stack.Screen name='index' options={{ 
                 title: 'Home', 
-                headerStyle: {backgroundColor: Colors.$backgroundPrimaryHeavy},
-                headerTintColor: Colors.$textDefaultLight, }} 
+            }} 
             />
             <Stack.Screen name='details' options={{ title: 'Workout Details'}} />
         </Stack>
