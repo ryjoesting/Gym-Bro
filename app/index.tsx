388f57e1972422141e33eac1bafb7ad989395d25
Ryan Joesting <rmjoesting@crimson.ua.edu>
2024-05-28 12:21:59 -0400

    Improve login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,35 @@
-import { View, Button, Text} from "react-native-ui-lib"
-import { Link } from "expo-router"
+import { View, Button, Text, TextField } from "react-native-ui-lib"
+import { router } from "expo-router"
+import { useState } from "react";
 
 // Main landing for app launch
 // Will need to check auth here, then skip to main application
 // Adding this for now, needs TODO later on
 
 export default function LoginScreen() {
+
+    const [email, onChangeEmail] = useState('');
+    const [pass, onChangePass] = useState('');
+
+    const handleLogin = () => {
+        router.replace('/(tabs)/home');
+    };
+
     return (
         <View flex style={{justifyContent: 'center',
                             alignItems: 'center'}}>
-            <Link asChild href={'/(tabs)/home'}>
-                <Button><Text white body>Login</Text></Button>
-            </Link>
+            <TextField
+                placeholder={'Email'}
+                onChangeText={onChangeEmail}
+                floatingPlaceholder
+            />
+            <TextField
+                placeholder={'Password'}
+                onChangeText={onChangePass}
+                floatingPlaceholder
+            />
+            <Button onPress={handleLogin}><Text white body>Login</Text></Button>
+            <Text>{email}</Text>
         </View>
     )
 }
